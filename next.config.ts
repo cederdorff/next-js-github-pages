40c2c6b9313e4fab8e1e3fe25456c2eb9b859cf2
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-29 11:10:19 +0100

    Update base path for GitHub Pages deployment in production environment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/next-js-github-pages"
+  output: "export"
 };
+
+// This is necessary for GitHub Pages to work correctly
+if (process.env.NODE_ENV === "production") {
+  nextConfig.basePath = "/nextjs-on-github-pages";
+}
 
 export default nextConfig;
