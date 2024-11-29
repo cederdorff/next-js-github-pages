93233cd897915965f0308373d5b6e4b78a885d69
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-29 11:08:56 +0100

    Add new SSG and build manifests; remove outdated static files and update base path for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "/next-js-github-pages"
 };
-
-// This is necessary for GitHub Pages to work correctly
-if (process.env.NODE_ENV === "production") {
-  nextConfig.basePath = "/nextjs-on-github-pages";
-}
 
 export default nextConfig;
