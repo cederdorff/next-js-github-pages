51c179cc4dee9ca5363d0600195392c761c9bcca
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-29 11:20:39 +0100

    Add post submission form and handler to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,18 @@ export default async function Home() {
 
   console.log(posts);
 
+  async function addPost(formData: FormData) {
+    "use server";
+    const caption = formData.get("caption");
+    const image = formData.get("image");
+    const uid = "ZfPTVEMQKf9vhNiUh0bj";
+    const createdAt = new Date().getTime();
+
+    const request = await fetch("https://next-post-app-race-default-rtdb.firebaseio.com/posts.json", {
+      method: "POST",
+      body: JSON.stringify({ caption, image, uid, createdAt })
+    });
+  }
   return (
     <main>
       <h1>Deploy your static Next.js site to GitHub Pages.</h1>
@@ -16,6 +28,12 @@ export default async function Home() {
           <li key={post.id}>{post.caption}</li>
         ))}
       </ul>
+
+      <form action={addPost}>
+        <input type="text" name="caption" />
+        <input type="url" name="image" />
+        <button type="submit">Add Post</button>
+      </form>
     </main>
   );
 }
